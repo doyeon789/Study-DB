6d704402268e465f7cfa3c51331d8e5c7bec206f
doyeon789 <ldyeon789@naver.com>
2026-04-29 10:55:14 +0900

    feat: 댓글삭제 url 작성

--- 03_01_db_many_to_one_relationships/articles/urls.py
+++ 03_01_db_many_to_one_relationships/articles/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:article_id>/delete/', views.delete, name='delete'),
     path('<int:article_id>/update/', views.update, name='update'),
-    path('<int:article_id>/comments/', views.comments_create, name='comments_create')
+    path('<int:article_id>/comments/', views.comments_create, name='comments_create'),
+    path('<int:article_id>/comments/<int:comment_id>/delete/',views.comments_delete,name='comments_delete')
 ]
